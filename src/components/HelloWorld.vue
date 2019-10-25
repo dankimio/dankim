527e2c2c78c95403cc50294ce7cfaa25c501ed36
Dan Kim <itsdanya@gmail.com>
2019-10-26 02:07:57 +0300

    Add some content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
-    <h1></h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, inventore. Cum, repudiandae.
+  <div class="container md:pt-48">
+    <h1 class="mb-5 text-5xl font-bold text-gray-900">
+      Hi, I'm Dan Kim.
+    </h1>
+    <p class="mb-5 text-3xl text-gray-700">
+      I write software and design interfaces.
     </p>
-    {{msg}}
+    <p class="mb-5 text-lg text-gray-800">
+      Currently based in Moscow, Russia
+    </p>
+
+    <div class="mb-6 border-b max-w-lg"></div>
+
+    <h2 class="text-lg text-gray-900 font-medium">
+      What I've been working on
+    </h2>
+
+    <div v-if="false">
+      {{msg}}
+    </div>
   </div>
 </template>
 
