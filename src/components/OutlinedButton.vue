925bef77f6837ed9048e435e58b9ce367f798c84
Dan Kim <itsdanya@gmail.com>
2019-10-26 18:46:14 +0300

    OutlinedButton: darker hover state

--- src/components/OutlinedButton.vue
+++ src/components/OutlinedButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="#" class="inline-block text-center px-3 py-1 border border-gray-400 text-gray-800 tracking-wide cursor-pointer hover:bg-gray-700 hover:text-white hover:border-gray-700">
+  <a href="#" class="inline-block text-center px-3 py-1 border border-gray-400 text-gray-800 tracking-wide cursor-pointer hover:bg-gray-800 hover:text-white hover:border-gray-800">
     <slot></slot>
   </a>
 </template>
