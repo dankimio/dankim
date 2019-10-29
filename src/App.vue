1271eb2e2d821be68ac10a480892962f4542275e
Dan Kim <itsdanya@gmail.com>
2019-10-30 00:59:40 +0300

    Adjust top padding on desktop screens

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="container px-6 pt-6 md:pt-32 lg:pt-40 md:max-w-lg lg:max-w-xl">
+  <div id="app" class="container px-6 pt-6 md:pt-32 md:max-w-lg lg:max-w-xl">
     <About />
 
     <Divider />
